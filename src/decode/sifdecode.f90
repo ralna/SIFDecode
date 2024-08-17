c5f642d4f9cda839cee42c2937569bf2c75c384c
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-08-17 13:38:35 -0400

    Release 2.6.0

--- src/decode/sifdecode.f90
+++ src/decode/sifdecode.f90
@@ -25,7 +25,7 @@
 !  V e r s i o n
 !---------------
 
-      CHARACTER ( LEN = 6 ) :: version = '2.5.1 '
+      CHARACTER ( LEN = 6 ) :: version = '2.6.0 '
 
 !--------------------
 !   P r e c i s i o n
