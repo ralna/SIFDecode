1550b0e1992e6255a63afba1980559a426da829d
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-09-11 05:38:29 -0500

    Update CI scripts

--- .github/julia/build_tarballs.jl
+++ .github/julia/build_tarballs.jl
@@ -48,7 +48,9 @@ platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
-   ExecutableProduct("sifdecoder_standalone", :sifdecoder_standalone),
+   ExecutableProduct("sifdecoder", :sifdecoder),
+   ExecutableProduct("clsf", :clsf),
+   ExecutableProduct("slct", :slct),
 ]
 
 # Dependencies that must be installed before this package can be built
